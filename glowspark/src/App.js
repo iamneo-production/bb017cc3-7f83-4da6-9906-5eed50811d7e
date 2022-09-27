593150cb29f7d8ce2016bdb95a53a6d84d34c237
Izroth404 <ranaavirup@gmail.com>
2022-09-27 17:09:27 +0000

    New Commit from Avirup

--- glowspark/src/App.js
+++ glowspark/src/App.js
@@ -1,16 +1,10 @@
-// import logo from './logo.svg';
-import React from "react";
-import { BrowserRouter as Router, Switch, Route,Routes } from "react-router-dom"
-import firstPage from './firstpage';
 import './App.css';
-
+import Register from './Register';
+import Dashboard from './components/Dashboard/Dashboard';
+import Preferences from './components/Preferences/Preferences'
 function App() {
   return (
-    <Router>
-      <Routes>
-            <Route exact path="/" element={<firstPage/>}/>
-      </Routes>
-    </Router>
+    <></>
   );
 }
 
---
+++ glowspark/src/components/Preferences/Preferences.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Preferences(){
+    return (
+        <h3>
+            Preferences
+        </h3> 
+    )
+}
